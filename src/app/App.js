c21f824f4a8e3d9d9c4333df9fb1c3a53b2ea6c1
Carlos Garcia <carlosgarcia@Carloss-MBP.home>
2019-03-06 16:50:31 +1100

    Added spotify api call in comments

--- src/app/App.js
+++ src/app/App.js
@@ -3,6 +3,11 @@ import './css/App.css';
 
 class App extends Component {
   render() {
+    //https://accounts.spotify.com/api/token
+    // Content-Type application/x-www-form-urlencoded 
+    // Auth Basic Yz...ZDY=      <- base64 encoded client id and secret   Basic <id:secret>
+
+    //BODY - grant_type : client_credentials. 
     return (
       <div className="App">
         
