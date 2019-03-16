11481137bf9c1a0399b6072afa37590102b92a2c
CARLOS <carlos@motusware.com>
2019-03-16 15:26:13 +1100

    reorganized files. Added services for API

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import './css/App.css';
+import MainContent from '../app/components/mainBody' 
 
 class App extends Component {
-  render() {
-    //https://accounts.spotify.com/api/token
-    // Content-Type application/x-www-form-urlencoded 
-    // Auth Basic Yz...ZDY=      <- base64 encoded client id and secret   Basic <id:secret>
 
-    //BODY - grant_type : client_credentials. 
+  render() {
     return (
       <div className="App">
-        
+        <MainContent/>
       </div>
     );
   }
--- src/app/components/mainBody.js
+++ src/app/components/mainBody.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './css/mainBody.css';
-import axios from 'axios'
+import { getAuth } from '../../services/spotifyAPI'
 
 class MainBody extends Component {
   
-  render() {
-    const API_END_POINT = process.env.API_END_POINT
-    //https://accounts.spotify.com/api/token
-    // Content-Type application/x-www-form-urlencoded 
-    // Auth Basic Yz...ZDY=      <- base64 encoded client id and secret   Basic <id:secret>
+  componentDidMount(){
+    getAuth()
+    .then(()=>{
 
-    //BODY - grant_type : client_credentials. 
+    })
+  }
+
+  render() {
     return (
       <div className="mainBody">
         
