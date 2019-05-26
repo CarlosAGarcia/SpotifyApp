be7517d813c30b826aa6b00d86ee4e7c3b461940
CARLOS <carlos@motusware.com>
2019-05-26 16:28:57 +1000

    npm install of styled components and jest

--- src/app/components/mainBody.js
+++ src/app/components/mainBody.js
@@ -6,7 +6,7 @@ class MainBody extends Component {
   
   componentDidMount(){
     getAuth()
-    .then(()=>{
+    .then(() => {
       console.log('in then of getAuth')
     })
   }
@@ -14,7 +14,7 @@ class MainBody extends Component {
   render() {
     return (
       <div className="mainBody">
-        
+        MAIN
       </div>
     );
   }
--- src/services/spotifyAPI.js
+++ src/services/spotifyAPI.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
-
+const authToken = process.env.API_TOKEN
+const authRoute = process.env.SPOTIFY_API_END_POINT
 export const getAuth = async () => {
     const url = 'https://accounts.spotify.com/api/token'
     const timeout = 1000
     const headers = {
-        'Authorization': 'Basic YzI1ZTEzMzAyMjkxNDc0Y2I3MWRhMmE5YzhmMjQyODE6NTRkZTI3YzM5Nzg1NDM3YjlhNjQ4MjRiMjViOWY1ZDY=', 
-        'Content-Type': 'application/x-www-form-urlencoded' 
+        'Authorization': authToken,//'Basic YzI1ZTEzMzAyMjkxNDc0Y2I3MWRhMmE5YzhmMjQyODE6NTRkZTI3YzM5Nzg1NDM3YjlhNjQ4MjRiMjViOWY1ZDY=', 
+        'Content-Type': authRoute//'application/x-www-form-urlencoded' 
     }
     const params = new URLSearchParams();
     params.append('grant_type', 'client_credentials');
